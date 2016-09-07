38e785ccec92de6e21ca20d4f40f279aa0536526
George Fung <gycf84@yahoo.com>
2016-09-06 17:37:10 -0700

    console log out vars
    instead of recreating a single key/field json, console out string with vars

--- test.js
+++ test.js
@@ -10,29 +10,33 @@ var test_json = {
   "array": [{'arraytest': "i am a string inside an object inside an array"},"test string in array"]
 }
 
+/**
+* JSON Reference
+*
+* Object.keys(json) returns an array of unnested keys
+*
+*/
+
 //recursive function
 function json_parsing(json_file) {
-  //loop the first layer and check for object types
-  var current_parent=[];
+  //loop the first level of keys and check if their value is an object type
   for (var i=0; i < Object.keys(json_file).length; i++){
     if(typeof json_file[Object.keys(json_file)[i]] == "object"){
       //we are here if this key's value is an object (array or json notation object)
-      //set a var to check the next nest
-      var nextlevel = json_file[Object.keys(json_file)[i]]
+      //set next nest key
+      var nextlevel = json_file[Object.keys(json_file)[i]];
       //recursively call self to keep going down the layers
-      json_parsing(nextlevel)
+      json_parsing(nextlevel);
     } else{
       //we are not an object
       //console.log out
       var key = Object.keys(json_file)[i];
       var value = json_file[Object.keys(json_file)[i]];
-      var test_object={}
-      test_object[key] = value
-      console.log("===============================")
-      console.log(test_object)
-      
+      console.log("key/pair ===============================")
+      console.log("{ \""+key+"\" : \""+value+"\" }")
     }
   }
 }
 
 json_parsing(test_json);
+
